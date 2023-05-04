0b908c36239cf0a8357775884078abaf844c3b27
muraliweb9 <muraliweb@hotmail.com>
2023-05-04 20:27:49 +0100

    first commit

---
+++ src/main/java/com/interview/test/carworkflowcloudapp/CarWorkflowCloudAppApplication.java
@@ -0,0 +1,13 @@
+package com.interview.test.carworkflowcloudapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarWorkflowCloudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarWorkflowCloudAppApplication.class, args);
+	}
+
+}
